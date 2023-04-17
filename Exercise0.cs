60e614bf1e714157fed083e857858d4584f92c2e
Laroub03 <ayoublaroub@gmail.com>
2023-04-17 22:53:36 +0200

    Initial commit

---
+++ Exercise0.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Threading;
+
+
+//class ThreadPoolDemo
+//{
+//    // Task1 method runs a string loop that is executed by a thread from the thread pool
+//    public void task1(object obj)
+//    {
+//        for (int i = 0; i <= 2; i++)
+//        {
+//            Console.WriteLine("Task 1 is being executed");
+//        }
+//    }
+
+//    // Task2 method runs a string loop that is executed by a thread from the thread pool
+//    public void task2(object obj)
+//    {
+//        for (int i = 0; i <= 2; i++)
+//        {
+//            Console.WriteLine("Task 2 is being executed");
+//        }
+//    }
+
+//    static void Main()
+//    {
+//        // Creating an instance of the ThreadPoolDemo class
+//        ThreadPoolDemo tpd = new ThreadPoolDemo();
+
+
+//        for (int i = 0; i < 2; i++)
+//        {
+//            // Queuing the task1 and task2 methods to the thread pool
+//            ThreadPool.QueueUserWorkItem(tpd.task1);
+//            ThreadPool.QueueUserWorkItem(tpd.task2);
+//        }
+
+//        Console.Read();
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ThreadPooling
+{
+
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            // Creating a Stopwatch object to measure time
+            Stopwatch mywatch = new Stopwatch();
+
+           
+            Console.WriteLine("Thread Pool Execution");
+
+            // Starting the stopwatch
+            mywatch.Start();
+
+            // Calling the ProcessWithThreadPoolMethod
+            ProcessWithThreadPoolMethod();
+
+            // Stopping the stopwatch
+            mywatch.Stop();
+
+            // Writing the elapsed time to the console
+            Console.WriteLine("Time consumed by ProcessWithThreadPoolMethod is : " + mywatch.ElapsedTicks.ToString());
+
+            // Resetting the stopwatch
+            mywatch.Reset();
+
+            Console.WriteLine("Thread Execution");
+            mywatch.Start();
+
+            // Calling the ProcessWithThreadMethod
+            ProcessWithThreadMethod();
+
+            mywatch.Stop();
+            Console.WriteLine("Time consumed by ProcessWithThreadMethod is : " + mywatch.ElapsedTicks.ToString());
+        }
+
+        // Defining a method to process the thread pool
+        static void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                // Queuing a user work item to the thread pool
+                ThreadPool.QueueUserWorkItem(Process);
+            }
+        }
+
+        // Defining a method to process threads
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                // Creating a new thread object and starting it
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+
+        // Defining a method to process the thread
+        static void Process(object callback)
+        {
+            // Creating a nested loop to simulate a long-running process
+            for (int i = 0; i < 100000; i++)
+            {
+                for (int j = 0; j < 100000; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
